3e8aee10282954948c460253d414dde50f244ec7
Vinícius <viniolicar2004@gmail.com>
2022-04-26 08:37:03 -0300

    Alguns ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ArchivedComponent } from './views/archived/archived.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 const routes: Routes = [
-  { path:'archived',
-  component:ArchivedComponent }
+  { path:'chat/:id',
+  component:ChatComponent },
+
 ];
 
 @NgModule({
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { DbService } from 'src/app/services/db.service';
 
 @Component({
@@ -6,38 +7,85 @@ import { DbService } from 'src/app/services/db.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
+
 export class ChatComponent implements OnInit {
-  @Input() infos:any
+  @Input() infos: any
 
-  messages:any [] = []
-  input:string = ''
+  messages: any[] = []
+  input: string = ''
+  id: any
+  messageId:any
 
-  constructor(private db:DbService) { }
+  constructor(private db: DbService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.db.messages().subscribe((infos:any) => {
-      infos.docs.map((doc:any) => {
-        this.messages.push(doc.data())
-      })
-      })
-      console.log(this.messages)
   }
-  
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.id = changes['infos'].currentValue.id
+    this.getMessage()
+  }
+
+  findId(message: any) {
+    // Procurar o id das mensagens
+    this.db.messages(this.id).subscribe(infos => {
+      const ids: any = infos.docs
+      const messages: any = ids.map((infos: any) => {
+        return infos.data().message
+      })
+
+      const index = messages.indexOf(message)
+      const id = ids[index]?.id
+      this.messageId = id
+    })
+  }
 
   sendMessage() {
     const time = new Date()
     const hours = time.getHours()
     const minutes = time.getMinutes()
 
+    // Messagem e suas informações
     const message = {
       message: this.input,
-      time: `${hours < 10? `0${hours}` : hours}:${minutes < 10? `0${minutes}` : minutes}`,
+      time: time,
       user: true,
       checked: false
     }
 
-    this.db.sendMessages(message).then(() => {
+    this.db.sendMessages(message, this.id).then(() => {
       window.location.reload()
     })
   }
+
+  getMessage() {
+    //Zera o array pra quando trocar de conversa não adicionar as mensagens anteriores com as novas.
+    this.messages = []
+
+    this.db.messages(this.id).subscribe((infos: any) => {
+      infos.docs.map((doc: any) => {
+        this.messages.push(doc.data())
+      })
+
+      //Vai organizar para a ultima menssagem enviada fique sempre embaixo
+      this.messages.sort((a:any, b:any):any => {
+        return a.time - b.time 
+      })
+    })
+  }
+
+  removeMessage() {
+    this.db.deleteMessage(this.id, this.messageId).then(() => {
+      window.location.reload()
+    })
+  }
+
+  transformTime(time:any) {
+    const date = new Date(time * 1000)
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
+
+    return `${hour < 10? `0${ hour }`: hour }:${minutes < 10? `0${ minutes }` : minutes }`
+  }
+
 }
